2a3a8ec947cdb048d5f486f9780dd70420ddc07d
Danny Yoo <dannyyoo@google.com>
2025-01-15 11:32:23 -0800

    More tests and correcting down index.

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -22,7 +22,7 @@ impl FieldMap {
         }
     }
 
-    fn trailheads(&self) -> impl Iterator<Item = usize> +'_ {
+    fn trailheads(&self) -> impl Iterator<Item = usize> + '_ {
         self.data
             .iter()
             .enumerate()
@@ -31,28 +31,28 @@ impl FieldMap {
     }
 
     fn directional_neighbors(&self, index: usize) -> Vec<usize> {
-	let mut results = Vec::new();
-	// right
-	if index % self.width + 1 < self.width && index + 1 < self.data.len() {
-	    results.push(index+1);
-	}
+        let mut results = Vec::new();
+        // right
+        if index % self.width + 1 < self.width && index + 1 < self.data.len() {
+            results.push(index + 1);
+        }
 
-	// left
-	if index % self.width > 0 && index != 0 {
-	    results.push(index - 1)
-	}
+        // left
+        if index % self.width > 0 && index != 0 {
+            results.push(index - 1)
+        }
 
-	// up
-	if index / self.width > 0 {
-	    results.push(index - self.width)
-	}
-	
-	// down
-	if index / self.width < self.height {
-	    results.push(index + self.width)
-	}
+        // up
+        if index / self.width > 0 {
+            results.push(index - self.width)
+        }
 
-	results
+        // down
+        if index / self.width < self.height - 1 {
+            results.push(index + self.width)
+        }
+
+        results
     }
 }
 
@@ -63,27 +63,45 @@ mod tests {
 
     #[gtest]
     fn test_trailheads() -> Result<()> {
-	let field = FieldMap::new("\
+        let field = FieldMap::new(
+            "\
 0123
 1234
 8765
 9876
-	    ");
-	verify_that!(field.trailheads().collect::<Vec<_>>(), [eq(&0)])
+	    ",
+        );
+        verify_that!(field.trailheads().collect::<Vec<_>>(), [eq(&0)])
     }
 
     #[gtest]
     fn test_directional_neighbors() -> Result<()> {
-	let field = FieldMap::new("\
+        let field = FieldMap::new(
+            "\
 0123
 1234
 8765
-	    ");
-	verify_that!(field.directional_neighbors(0), [eq(&1), eq(&4)])?;
-	verify_that!(field.directional_neighbors(5), [eq(&6), eq(&4), eq(&1), eq(&9)])?;
-	Ok(())
-    }
+	    ",
+        );
+        verify_that!(
+            field.directional_neighbors(0),
+            unordered_elements_are![eq(&1), eq(&4)]
+        )?;
+        verify_that!(
+            field.directional_neighbors(5),
+            unordered_elements_are![eq(&6), eq(&4), eq(&1), eq(&9)]
+        )?;
+        verify_that!(
+            field.directional_neighbors(11),
+            unordered_elements_are![eq(&10), eq(&7)]
+        )?;
+	verify_that!(
+	    field.directional_neighbors(10),
+            unordered_elements_are![eq(&9), eq(&11), eq(&6)]
+        )?;
 
+        Ok(())
+    }
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
