11e1f6dc901a204d4c90330e3153f5db2502e778
Danny Yoo <dannyyoo@google.com>
2025-01-21 10:39:30 -0800

    Finished part 2 for day 13.
    Did the linear algebra approach, which scales to part 2.  Generalized
    the part_1/part_2 functions to take in parameterized solver to make it
    easier to experiment.

--- src/bin/day13.rs
+++ src/bin/day13.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
 use nom::bytes::complete::tag;
-use nom::character::complete::{alpha1, line_ending, u64};
+use nom::character::complete::{alpha1, line_ending, i64};
 use nom::multi::{many1, separated_list0};
 use nom::IResult;
 use std::cmp::{Ord, PartialOrd, Reverse};
@@ -10,7 +10,7 @@ use std::error::Error;
 use std::ops::{Add, Sub};
 
 #[derive(Debug, PartialEq, PartialOrd, Ord, Eq, Copy, Clone)]
-struct Point(u64, u64);
+struct Point(i64, i64);
 
 impl Add for Point {
     type Output = Self;
@@ -52,12 +52,12 @@ impl Sub<&Point> for Point {
 /// pressed]` the same as `[a pressed, a pressed, b pressed]`.  So we
 /// design the possible actions so that we keep a canonical sequence,
 /// given the order independence between the button presses.
-fn dijkstra_solver(a: &Point, b: &Point, prize: &Point) -> Option<u64> {
+fn dijkstra_solver(a: &Point, b: &Point, prize: &Point) -> Option<i64> {
     let mut heap = BinaryHeap::new();
 
     #[derive(Debug, PartialEq, PartialOrd, Ord, Eq)]
     struct State {
-        tokens: u64,
+        tokens: i64,
         point: Point,
     }
 
@@ -90,6 +90,37 @@ fn dijkstra_solver(a: &Point, b: &Point, prize: &Point) -> Option<u64> {
         }
     }
     None
+}
+
+/// We want to find m, n such that for given points A, B, and P,
+///
+///     m * [a0, a1] + n * [b0, b1] = [p0, p1]
+///
+/// If we write out the math, solutions are:
+///
+///    m = (b0 * p1 - b1 * p0) / (a1 * b0 - a0 * b1)
+///    n = (a1 * p0 - a0 * p1) / (a1 * b0 - a0 * b1)
+fn linear_algebra_solver(a: &Point, b: &Point, p: &Point) -> Option<i64> {
+    let divisor = (a.1 * b.0).checked_sub(a.0 * b.1).expect("underflow");
+    if divisor == 0 {
+        // In this case, we'd have to do something with diophantine equations.
+        // https://en.wikipedia.org/wiki/Diophantine_equation#One_equation
+        // 
+        // For now, we give up, as it appears that the test data
+        // doesn't hit this case.
+        panic!("divisor zero");
+    }
+
+    let m_numerator = b.0 * p.1 - b.1 * p.0;
+    let n_numerator = a.1 * p.0 - a.0 * p.1;
+
+    if m_numerator % divisor == 0 && n_numerator % divisor == 0 {
+        Some((3 * m_numerator + n_numerator) / divisor)
+    } else {
+        // We give up on non-integer solutions, as that means there's
+        // no way to reach the prize.
+        None
+    }
 }
 
 #[cfg(test)]
@@ -154,6 +185,32 @@ Prize: X=8400, Y=5400",
 
         Ok(())
     }
+
+
+    #[gtest]
+    fn test_linear_algebra_solver_small() -> Result<()> {
+        verify_that!(
+            linear_algebra_solver(&Point(94, 34), &Point(22, 67), &Point(8400, 5400)),
+            some(eq(280))
+        )?;
+
+        verify_that!(
+            linear_algebra_solver(&Point(26, 66), &Point(67, 21), &Point(12748, 12176)),
+            none()
+        )?;
+
+        verify_that!(
+            linear_algebra_solver(&Point(17, 86), &Point(84, 37), &Point(7870, 6450)),
+            some(eq(200))
+        )?;
+
+        verify_that!(
+            linear_algebra_solver(&Point(64, 23), &Point(27, 71), &Point(18641, 10279)),
+            none()
+        )?;
+
+        Ok(())
+    }
 }
 
 fn parse_button(input: &str) -> IResult<&str, (&str, Point)> {
@@ -161,18 +218,18 @@ fn parse_button(input: &str) -> IResult<&str, (&str, Point)> {
     // eat A or B
     let (input, name) = alpha1(input)?;
     let (input, _) = tag(": X+")(input)?;
-    let (input, x) = u64(input)?;
+    let (input, x) = i64(input)?;
     let (input, _) = tag(", Y+")(input)?;
-    let (input, y) = u64(input)?;
+    let (input, y) = i64(input)?;
 
     Ok((input, (name, Point(x, y))))
 }
 
 fn parse_prize(input: &str) -> IResult<&str, Point> {
     let (input, _) = tag("Prize: X=")(input)?;
-    let (input, x) = u64(input)?;
+    let (input, x) = i64(input)?;
     let (input, _) = tag(", Y=")(input)?;
-    let (input, y) = u64(input)?;
+    let (input, y) = i64(input)?;
     Ok((input, Point(x, y)))
 }
 
@@ -189,14 +246,20 @@ fn parse_all_claws(s: &str) -> IResult<&str, Vec<(Point, Point, Point)>> {
     separated_list0(many1(line_ending), parse_claw)(s)
 }
 
-fn part_1(claws: &[(Point, Point, Point)]) -> u64 {
+fn part_1(
+    claws: &[(Point, Point, Point)],
+    solver: impl Fn(&Point, &Point, &Point) -> Option<i64>,
+) -> i64 {
     claws
         .into_iter()
-        .filter_map(|(a, b, prize)| dijkstra_solver(a, b, prize))
+        .filter_map(|(a, b, prize)| solver(a, b, prize))
         .sum()
 }
 
-fn part_2(claws: &[(Point, Point, Point)]) -> u64 {
+fn part_2(
+    claws: &[(Point, Point, Point)],
+    solver: impl Fn(&Point, &Point, &Point) -> Option<i64>,
+) -> i64 {
     claws
         .into_iter()
         .map(|(a, b, prize)| {
@@ -206,15 +269,16 @@ fn part_2(claws: &[(Point, Point, Point)]) -> u64 {
                 Point(prize.0 + 10000000000000, prize.1 + 10000000000000),
             )
         })
-        .filter_map(|(a, b, prize)| dijkstra_solver(a, b, &prize))
+        .filter_map(|(a, b, prize)| solver(a, b, &prize))
         .sum()
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
     let input = std::io::read_to_string(std::io::stdin())?;
-    let (input, claws) = parse_all_claws(&input).map_err(|e| e.to_owned())?;
+    let (_, claws) = parse_all_claws(&input).map_err(|e| e.to_owned())?;
 
-    println!("Part 1: {}", part_1(&claws));
+    println!("Part 1: dijkstra {}", part_1(&claws, dijkstra_solver));
+    println!("Part 1: linear {}", part_1(&claws, linear_algebra_solver));
 
     // Essentially, we're trying to find naturals n1, n2 such that
     //    n1 * A + n2 * B = prize
@@ -231,6 +295,6 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Cost(n1, n2) * B = 3 * n1 * B + n2 * B
     // ==> Cost(n1) * B = 3 * n1 * B + (prize - n1 * A)
 
-    println!("Part 2: {}", part_2(&claws));
+    println!("Part 2: {}", part_2(&claws, linear_algebra_solver));
     Ok(())
 }
