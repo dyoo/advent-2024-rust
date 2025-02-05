a6281dde2e3ebb7f3960ac5d6f62610e606d8c95
Danny Yoo <dyoo@hashcollision.org>
2025-02-04 22:25:36 -0800

    Finished part 1 for day 18.

--- src/bin/day18.rs
+++ src/bin/day18.rs
@@ -1,3 +1,5 @@
+use advent_2024::{Direction, TileIndex};
+
 mod parser {
     use nom::bytes::complete::tag;
     use nom::character::complete::{line_ending, u8};
@@ -39,10 +41,107 @@ mod parser {
     }
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>>{
+struct Grid {
+    data: Vec<bool>,
+    tile_index: TileIndex,
+}
+
+impl Grid {
+    fn new(width: u8, height: u8) -> Self {
+        Self {
+            data: vec![false; width as usize * height as usize],
+            tile_index: TileIndex {
+                width: width as usize,
+                height: height as usize,
+            },
+        }
+    }
+    fn height(&self) -> usize {
+        self.tile_index.height
+    }
+
+    fn mark(&mut self, coord: (u8, u8)) {
+        let index = self.height() * coord.1 as usize + coord.0 as usize;
+        self.data[index] = true;
+    }
+
+    fn step_count(&self) -> Option<u32> {
+        let mut visited = vec![false; self.data.len()];
+        let mut to_visit = vec![0];
+        let mut count = 0;
+        while !to_visit.is_empty() {
+            let mut to_visit_next = Vec::new();
+
+            for index in to_visit {
+                if visited[index] {
+                    continue;
+                }
+                visited[index] = true;
+                if index == visited.len() -1  {
+                    return Some(count);
+                }
+                
+                for dir in [
+                    Direction::Left,
+                    Direction::Right,
+                    Direction::Up,
+                    Direction::Down,
+                ] {
+                    to_visit_next.extend(
+                        self.tile_index
+                            .dir_to(index, dir)
+                            .filter(|idx| !visited[*idx] && !self.data[*idx]),
+                    );
+                }
+            }
+            count += 1;            
+            to_visit = to_visit_next;
+        }
+        
+        None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use googletest::prelude::*;
+
+    #[gtest]
+    fn test_step_count() -> Result<()> {
+        let data = "\
+5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+";
+        let (_, coords): (_, Vec<(u8, u8)>) =
+            parser::parse_coords(data).map_err(|e| e.to_owned())?;
+        let mut grid = Grid::new(7, 7);
+        for c in coords {
+            grid.mark(c);
+        }
+        verify_that!(grid.step_count(), some(eq(22)))
+    }
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::io::read_to_string(std::io::stdin())?;
-    let coords = parser::parse_coords(&input).map_err(|e| e.to_owned())?;
-    dbg!(&coords);
+    let (_, coords): (_, Vec<(u8, u8)>) = parser::parse_coords(&input).map_err(|e| e.to_owned())?;
+    let mut grid = Grid::new(71, 71);
+    for c in &coords[..1024] {
+        grid.mark(*c);
+    }
+
+    println!("Part 1: {:?}", grid.step_count());
+    
     Ok(())
 }
- 
