6c89a9e9204dafbf8584584dcd9db0ede7ca83ba
Danny Yoo <dannyyoo@google.com>
2025-02-09 13:14:23 -0800

    Parser done.

--- src/bin/day19.rs
+++ src/bin/day19.rs
@@ -26,14 +26,36 @@ impl TryFrom<char> for Color {
 }
 
 // A choice is a slice of colors.
-type Choice = Box<[Color]>;
+type ColorString = Box<[Color]>;
 
-fn parse_choice(s: &str) -> Result<Choice, Box<dyn Error>> {
+fn parse_color_string(s: &str) -> Result<ColorString, Box<dyn Error>> {
     s.chars().map(Color::try_from).collect()
 }
 
-fn parse_choices(s: &str) -> Result<Box<[Choice]>, Box<dyn Error>> {
-    s.split(", ").map(parse_choice).collect()
+fn parse_choices(s: &str) -> Result<Box<[ColorString]>, Box<dyn Error>> {
+    s.split(", ").map(parse_color_string).collect()
+}
+
+#[derive(Debug, PartialEq)]
+struct Problem {
+    choices: Box<[ColorString]>,
+    designs: Box<[ColorString]>,
+}
+
+fn parse_problem(s: &str) -> Result<Problem, Box<dyn Error>> {
+    let mut items = s.split("\n\n");
+    let choices = items
+        .next()
+        .map(parse_choices)
+        .ok_or::<Box<dyn Error>>("Missing choices".into())??;
+    let designs = items
+        .next()
+        .ok_or::<Box<dyn Error>>("Missing designs".into())?
+        .lines()
+        .map(parse_color_string)
+        .collect::<Result<Vec<_>, _>>()?
+        .into();
+    Ok(Problem { choices, designs })
 }
 
 #[cfg(test)]
@@ -47,18 +69,22 @@ mod tests {
         verify_that!(
             parse_choices(s).into_test_result()?,
             elements_are![
-              container_eq([Color::R].into()),
-              container_eq([Color::W, Color::R].into()),
-              container_eq([Color::B].into()),
-              container_eq([Color::G].into()),
-              container_eq([Color::B, Color::W, Color::U].into()),
-              container_eq([Color::R, Color::B].into()),
-              container_eq([Color::G, Color::B].into()),
-              container_eq([Color::B, Color::R].into()),
+                container_eq([Color::R].into()),
+                container_eq([Color::W, Color::R].into()),
+                container_eq([Color::B].into()),
+                container_eq([Color::G].into()),
+                container_eq([Color::B, Color::W, Color::U].into()),
+                container_eq([Color::R, Color::B].into()),
+                container_eq([Color::G, Color::B].into()),
+                container_eq([Color::B, Color::R].into()),
             ]
         )?;
         Ok(())
     }
 }
 
-fn main() {}
+fn main() -> Result<(), Box<dyn Error>>{
+  let problem = parse_problem(&std::io::read_to_string(std::io::stdin())?)?;
+
+  Ok(())
+}
